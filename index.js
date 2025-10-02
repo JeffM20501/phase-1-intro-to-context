b1b8bc4b905a84ed624b60df06f8a873ce4d17b2
JeffM20501 <jeff.muna@student.moringaschool.com>
2025-10-02 17:03:13 +0300

    compeleted lab

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function createEmployeeRecord([firstName, familyName, title, payPerHour]){
 }
 
 const empo = createEmployeeRecord(['jeff', 'Muna', 'Hr', 20])
-
+const empo4=createEmployeeRecords([['jeff', 'Muna', 'Hr', 20], ['Jack', 'Mainia', 'Hr', 20]])
+const empo2 = createTimeInEvent(createEmployeeRecord(['jeff', 'Muna', 'Hr', 20]), "2025-10-20 1300")
+const empo3 = createTimeOutEvent(empo2, "2025-10-20 1900")
 // console.log(createEmployeeRecord(['jeff', 'Muna', 'Hr', 20]))
 
 function createEmployeeRecords(arr){
@@ -44,13 +46,13 @@ function createTimeOutEvent(obj, dateStamp){
 }
 
 function hoursWorkedOnDate(obj, dateStamp){
-    const [date, hour] = dateStamp.split(' ')
+    const [date] = dateStamp.split(' ')
     const timeIn = obj.timeInEvents.find(event=>event.date===date)
     const timeOut = obj.timeOutEvents.find(event=>event.date===date)
     return (timeOut.hour-timeIn.hour)/100
 }
 
-// console.log(hoursWorkedOnDate(empo, '2025-10-20 1300'))
+// console.log(hoursWorkedOnDate(empo3, '2025-10-20 1300'))
 
 function wagesEarnedOnDate(obj, dateStamp){
     const payOwned = hoursWorkedOnDate(obj, dateStamp)*obj.payPerHour
@@ -58,5 +60,22 @@ function wagesEarnedOnDate(obj, dateStamp){
 }
 
 function allWagesFor(obj){
-    
+    let wage=0
+    for(const date of obj.timeInEvents){
+        wage += wagesEarnedOnDate(obj, date.date)
+    }
+    return wage
 }
+
+function calculatePayroll(arr){
+    let payRoll=0
+    for(const pay of arr){
+        for(const dates of pay.timeInEvents){
+            payRoll += wagesEarnedOnDate(pay, dates.date)
+        }
+    }
+    return payRoll
+}
+// console.log(empo4)
+
+console.log(calculatePayroll(empo4))
